52a27aa0f00e27d641929f9104452cce808c141b
Jack Fink <jfink@its4logistics.com>
2023-04-18 23:14:16 -0400

    CommandHandler

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+    
+    public static Task Main(string[] args) => new Program().MainAsync();
+    
+    private async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        var token = File.ReadAllText("../../../bot_token");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
